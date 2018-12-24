5f7b2543df5e4c452e6c089faddd532fd0997af2
七彩blog <35214016+lizhongnian@users.noreply.github.com>
2018-12-25 00:09:50 +0800

    Update serverstatus.js

--- web/js/serverstatus.js
+++ web/js/serverstatus.js
@@ -136,10 +136,10 @@ function uptime() {
 			// Ipstatus
 			if (result.servers[i].ip_status) {
 				TableRow.children["ip_status"].children[0].children[0].className = "progress-bar progress-bar-success";
-				TableRow.children["ip_status"].children[0].children[0].innerHTML = "<small>MH361</small>";
+				TableRow.children["ip_status"].children[0].children[0].innerHTML = "<small>OnLine</small>";
 			} else {
 				TableRow.children["ip_status"].children[0].children[0].className = "progress-bar progress-bar-danger";
-				TableRow.children["ip_status"].children[0].children[0].innerHTML = "<small>MH370</small>";
+				TableRow.children["ip_status"].children[0].children[0].innerHTML = "<small>OffLine</small>";
 			}
 
 			// Name
